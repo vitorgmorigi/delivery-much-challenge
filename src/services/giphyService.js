89090506bc97c912844336fe0ecc0e78a51603d3
vitorgmorigi <vitorgarcezmorigi@gmail.com>
2020-04-25 17:18:42 -0300

    Adds ESlint to the project.

--- src/services/giphyService.js
+++ src/services/giphyService.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 module.exports = {
     async findGifByWord (word) {
         try {
-           const giphyApiResponse = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${word}&limit=1`);
+           const giphyApiResponse = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${global.process.env.GIPHY_API_KEY}&q=${word}&limit=1`);
            const gif = giphyApiResponse.data.data.length ? giphyApiResponse.data.data[0] : {};
            
            return gif;
